1b0cd654619e19ff23c95dc30f4d0232860bce93
mrrocky <mrrocky@mrpc>
2021-12-25 22:04:23 -0500

    comit

--- 2021/day3/p1.py
+++ 2021/day3/p1.py
@@ -1,7 +1,16 @@
 file = open('input.txt' , 'r')
 input = file.read()
 file.close()
-input=input.split('\n').split('')
+input=input.split('\n')
 gamma=[]
-num=[0,0,0,0,0,0,0,0,0,0,0,0] 
-print (input) 
+num=[[],[],[],[],[],[],[],[],[],[],[],[]] 
+for x in input:
+    o=0
+    for i in x:
+        num[o].append(i)
+        o+=1
+
+print (num)
+
+
+
