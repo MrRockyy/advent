5d571c02458a35bd9b4f981406573c7d44e3ec35
mrrocky <mrrocky@mrpc>
2021-12-25 20:35:39 -0500

    comit

--- 2021/p2day1.py
+++ 2021/p2day1.py
@@ -10,7 +10,9 @@ for x in input:
     a=int(input[y])+int(input[y+1])+int(input[y+2])
     b=int(input[y+3])+int(input[y+4])+int(input[y+5])
     inx=input.index(input[y+5])
-    y=inx + 1 
+    y=inx + 1
+    print(a)
+    print(b)
     if b > a:
         count+=1
 print(count)
