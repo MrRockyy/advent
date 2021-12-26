4eb5b1b2516f70957f10643fbf7948a8b1963e8c
mrrocky <mrrocky@mrpc>
2021-12-25 21:02:48 -0500

    comit

--- 2021/p2day1.py
+++ 2021/p2day1.py
@@ -9,7 +9,7 @@ input.remove('')
 holi  = [1,2,1,12,12,23,21,3,123,123,213,123]
 print (len(input))
 for x in input:
-    if y+5 >= len(input):
+    if y >= len(input):
         break
     a=int(input[y])+int(input[y+1])+int(input[y+2])
     b=int(input[y+1])+int(input[y+2])+int(input[y+3])
