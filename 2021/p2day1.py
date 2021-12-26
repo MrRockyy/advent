984e90acd05d31d72c8a767eb5a0b4dff6dfeb1d
mrrocky <mrrocky@mrpc>
2021-12-25 20:26:27 -0500

    comit

--- 2021/p2day1.py
+++ 2021/p2day1.py
@@ -2,14 +2,14 @@
 file = open('input.txt' , 'r')
 input = file.read()
 file.close()
-y=1
+y=0
 count=0
 input=input.split('\n')
 input.remove('')
 for x in input:
-    if y == len(input):
-        break
-    if int(x) < int(input[y]):
-        count+=1
-    y+=1
+    a=int(input[y])+int(input[y+1])+int(input[y+2])
+    b=int(input[y+3])+int(input[y+4])+int(input[y+5])
+    inx=input.index(input[y+5])
+    y=inx+1
+    print(y)
 print(count)
