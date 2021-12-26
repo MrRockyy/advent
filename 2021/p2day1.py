0f06e87b586fe585e5be1b7b55641ef180947618
mrrocky <mrrocky@mrpc>
2021-12-25 20:18:20 -0500

    comit

---
+++ 2021/p2day1.py
@@ -0,0 +1,15 @@
+
+file = open('input.txt' , 'r')
+input = file.read()
+file.close()
+y=1
+count=0
+input=input.split('\n')
+input.remove('')
+for x in input:
+    if y == len(input):
+        break
+    if int(x) < int(input[y]):
+        count+=1
+    y+=1
+print(count)
