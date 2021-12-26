a70e300bb14fc3b2b0cf39a05a85ee10921edf90
mrrocky <mrrocky@mrpc>
2021-12-25 21:13:27 -0500

    comit

---
+++ 2021/day2/p1.py
@@ -0,0 +1,22 @@
+file = open('input.txt' , 'r')
+input = file.read()
+file.close()
+input=input.split('\n')
+y=0
+x=0 
+for direction in input:
+    direction=direction.split(' ')
+    if direction[0] == 'forward':
+        x+=int(direction[1])
+
+    elif direction[0] == 'down':
+        y-=int(direction[1]) 
+
+    elif direction[0] == 'up':
+        y+=int(direction[1])
+
+print(x*y)
+    
+
+
+
