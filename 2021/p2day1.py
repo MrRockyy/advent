7eec6483783e5aad900dfe21a811e7e406051bb2
mrrocky <mrrocky@mrpc>
2021-12-25 20:29:30 -0500

    comit

--- 2021/p2day1.py
+++ 2021/p2day1.py
@@ -9,8 +9,8 @@ input.remove('')
 for x in input:
     a=int(input[y])+int(input[y+1])+int(input[y+2])
     b=int(input[y+3])+int(input[y+4])+int(input[y+5])
-    inx=input.index(input[y+5])
-    y=inx+1
+    inx=input.index(input[y])
+    y=inx
     print(inx)
     break
 print(count)
