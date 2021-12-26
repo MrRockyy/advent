26fdaea6f3b0403071c60edcf71077b911b1382b
mrrocky <mrrocky@mrpc>
2021-12-25 20:37:40 -0500

    comit

--- 2021/p2day1.py
+++ 2021/p2day1.py
@@ -6,13 +6,14 @@ y=0
 count=0
 input=input.split('\n')
 input.remove('')
-for x in input:
+holi  = [1,2,1,12,12,23,21,3,123,123,213,123]
+for x in holi:
     a=int(input[y])+int(input[y+1])+int(input[y+2])
     b=int(input[y+3])+int(input[y+4])+int(input[y+5])
     inx=input.index(input[y+5])
     y=inx + 1
-    print(a)
-    print(b)
+    print(y)
+    
     if b > a:
         count+=1
 print(count)
