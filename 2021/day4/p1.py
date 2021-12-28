17bf0f5d01cf70f8b7f5a81320a175204592375b
mrrocky <mrrocky@mrpc>
2021-12-27 21:46:05 -0500

    comit

--- 2021/day4/p1.py
+++ 2021/day4/p1.py
@@ -4,7 +4,7 @@ file.close()
 input=input.split('\n')
 table=[]
 bingo = []
-x=0
+x=-1
 for i in range (1000):
     try: 
       input.remove( '')
@@ -20,7 +20,7 @@ for i in input:
     if i != input[0]:
        table.append(i)
     if x == 4:
-        x=1
+        x=0
         bingo.append(table)
         table=[]
 
