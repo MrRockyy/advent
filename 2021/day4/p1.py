5985ce3cb9604cc227a17a0f47ca69ea14010147
mrrocky <mrrocky@mrpc>
2021-12-27 21:58:44 -0500

    comit

--- 2021/day4/p1.py
+++ 2021/day4/p1.py
@@ -8,7 +8,7 @@ x=-1
 for i in range (1000):
     try: 
       input.remove( '')
-    except ValueError: 
+    except valueerror: 
         pass
 
 
@@ -17,12 +17,19 @@ for i in range (1000):
 for i in input:
     x+=1
     i=i.split(' ')
+    for i in range (10):
+       try: 
+          i.remove( '')
+       except valuerror: 
+         pass
+
+
     if i != input[0]:
+
        table.append(i)
     if x == 4:
         x=0
         bingo.append(table)
         table=[]
-print(bingo[0])
 
       
